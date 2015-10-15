a906a1356d3e2b24ebe95e42e302b530bab7e07a
walke <diesou9@gmail.com>
2015-10-15 12:11:41 +0200

    manifest file update

--- app/src/main/java/no/ice_9/xquisite/PlayerActivity.java
+++ app/src/main/java/no/ice_9/xquisite/PlayerActivity.java
@@ -1,32 +1,22 @@
 package no.ice_9.xquisite;
 
-import android.app.Activity;
-
-import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-
-public class MainActivity extends Activity {
-
-    //Start new activity for creating new part of a story.
-    public void CreateNewStory(View view)
-    {
-        Intent intent = new Intent(this, PlayerActivity.class);
-    }
+public class PlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_player);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_player, menu);
         return true;
     }
 
