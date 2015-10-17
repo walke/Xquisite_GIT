d536a2309418a243bae5889cb00055f3eeeda044
walke <diesou9@gmail.com>
2015-10-17 12:55:10 +0200

    Recorder init functions + Preview class added

---
+++ app/src/main/java/no/ice_9/xquisite/Preview.java
@@ -0,0 +1,7 @@
+package no.ice_9.xquisite;
+
+/**
+ * Created by HUMAN on 17.10.2015.
+ */
+public class Preview {
+}
--- app/src/main/java/no/ice_9/xquisite/RecorderActivity.java
+++ app/src/main/java/no/ice_9/xquisite/RecorderActivity.java
@@ -1,16 +1,105 @@
 package no.ice_9.xquisite;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class RecorderActivity extends AppCompatActivity {
+
+    //CLASS VARIABLES
+    Camera mCamera;//Deprecated.. don't know yet what to do about it
+    Preview mPreview;
+
+    //INIT CAMERA
+    private boolean initCamera(int camId)
+    {
+        boolean result;
+        if(mCamera==null)
+        {
+
+
+
+            //try to get camera instance
+            mCamera=getCameraInstance(camId);
+            if(mCamera==null){return false;}
+            else
+            {
+                Log.d("VIDEO_LOG", "got Camera instance");
+            }
+
+            //try to get preview
+            result = initPreview();
+            if(!result){return false;}
+            else{Log.d("VIDEO_LOG","got Preview");}
+
+            mTimeLeft=10;
+
+
+
+            new Timer().scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    mTimeLeft--;
+                    if(mTimeLeft<=0){this.cancel();}
+                    tAct.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mRecorderTimeText.setText(""+mTimeLeft);
+
+                            if(mTimeLeft<=0){forceStartCapture();}
+                        }
+                    });
+
+
+
+                }
+            },0,1000);
+
+
+
+
+        }
+        return true;
+    }
+
+    //INIT PREVIEW
+    private void releasePreview()
+    {
+
+        if (mPreview != null) {
+            Log.d("RECORDER", "releasing preview ");
+            mPreview = null;
+            Log.d("RECORDER", "preview released ");
+
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(int camId){
+        Camera c = null;
+        try {
+            c = Camera.open(camId); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recorder);
+
+        //INIT CAMERA AND ALL IT DEPENDS ON
+        initCamera(1);//for now camId = 1; asuming front facing camera.
+        // Will later be fixed to search for frontfacing camera
     }
 
     @Override
