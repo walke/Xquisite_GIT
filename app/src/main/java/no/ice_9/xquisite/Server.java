a838ebc2ca7a99ef3ca8ad496b974d0a8c65009c
walke <diesou9@gmail.com>
2015-10-17 18:11:07 +0200

    server connection setup

--- app/src/main/java/no/ice_9/xquisite/Server.java
+++ app/src/main/java/no/ice_9/xquisite/Server.java
@@ -3,6 +3,13 @@ package no.ice_9.xquisite;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by HUMAN on 15.10.2015.
  */
@@ -11,6 +18,7 @@ public class Server {
     static String CODE_CHECK_CONNECTION="23354700000";//check connection
 
     String adress;
+    int serverResponseCode = 0;
 
     Server(Context context)
     {
@@ -27,13 +35,94 @@ public class Server {
 
         boolean result=false;
 
-        if(response=="1"){result=true;};
+        if(response=="sucess"){result=true;};
 
         return result;
     }
 
     private String postToServer(String code,String data)
     {
-        return "1";
+        HttpURLConnection conn = null;
+        String result="-1";
+
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+        try
+        {
+            URL url = new URL(adress+"comm/");
+            conn = (HttpURLConnection) url.openConnection();
+
+            conn.setDoInput(true); // Allow Inputs
+            conn.setDoOutput(true); // Allow Outputs
+            conn.setUseCaches(false); // Don't use a Cached Copy
+
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+            DataOutputStream outSt = new DataOutputStream(conn.getOutputStream());
+
+            outSt.writeBytes(twoHyphens + boundary + lineEnd);
+            outSt.writeBytes("Content-Disposition: form-data; name='code'" + lineEnd + lineEnd);
+            outSt.writeBytes(code + lineEnd);
+
+            outSt.writeBytes(twoHyphens + boundary + lineEnd);
+            outSt.writeBytes("Content-Disposition: form-data; name='data'" + lineEnd + lineEnd);
+            outSt.writeBytes(data + lineEnd);
+
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+
+            String inS="";
+            char A;
+            boolean done=false;
+            int iter=0;
+            while(!done)
+            {
+                iter++;
+                A=(char)in.read();
+                //Log.d("SERVER","reading: "+A);
+                if(A==36 || iter>1000)
+                {
+                    done=true;
+                }
+                else
+                {
+                    inS=inS+A;
+                }
+
+                //inS=inS+in.read();
+
+            }
+            result=inS;
+
+            serverResponseCode = conn.getResponseCode();
+
+            String serverResponseMessage = conn.getResponseMessage();
+            Log.d("SERVER", "resp code:" + serverResponseCode);
+            Log.d("SERVER", "resp msg:" + serverResponseMessage);
+
+        }
+        catch (MalformedURLException ex)
+        {
+
+        }
+        catch (Exception e)
+        {
+
+            Log.e("SERVER","FUCK");
+            //dialog.dismiss();
+            e.printStackTrace();
+
+
+
+        }
+        finally {
+            if(conn!=null)
+            conn.disconnect();}
+
+        return result;
     }
 }
